a5b4ded1f6d9f30aaa0215a09590a1d1ec281886
JustaMask <91282404+JustaMask@users.noreply.github.com>
2025-06-06 17:03:49 +0300

    Revamp commit

--- Iris-flower-classification.py
+++ Iris-flower-classification.py
@@ -1,78 +1,32 @@
-# the library we use to download datasets from kaggle
-from kaggle.api.kaggle_api_extended import KaggleApi
+from Helper.AI_Helper import *
+from sklearn.tree import DecisionTreeClassifier , plot_tree
 
-# actual machine learning models are here
-from sklearn.linear_model import LogisticRegression
 
-# we use this to split the dataset
-from sklearn.model_selection import train_test_split
+Iris_dataset = Get_Dataset('himanshunakrani/iris-dataset', 'iris.csv')
 
-# makes the values range between 0 and 1
-from sklearn.preprocessing import StandardScaler
-
-# we are going to access our data set through this library
-import pandas as pd
-
-# will use this later to output the predictions
-import numpy as np
-
-# will use this to plot the dataset values and predictions
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# this will connect you to kaggle so you can download datasets from it
-api = KaggleApi()
-api.authenticate()
-
-# you only need to do this once and then remove this line after the dataset is in the project directory
-
-api.dataset_download_file('himanshunakrani/iris-dataset',
-                          file_name='iris.csv')
-
-# this is your dataset
-Iris_dataset = pd.read_csv('iris.csv')
-
-# this is the value we want to predict
+# target value
 y = Iris_dataset['species']
 
-# after saving the value we want to predict we drop it from our dataset
+# removing it from the dataset
 x = Iris_dataset.drop('species', axis=1)
 
-# split the training data into 4 variables
-x_train , x_val , train_y , val_y = train_test_split(x, y, test_size=0.2, random_state=0)
+# split the dataset into train and test
+x_train , x_test , y_train , y_test = train_test_split(x, y, test_size=0.2, random_state=0,stratify=y)
 
-# normalize all the values between 0 and 1 for all float values, in test and validation
-x_train = StandardScaler().fit_transform(x_train)
-x_val = StandardScaler().fit_transform(x_val)
-
-# make the model (will talk more about it on later chapters , but for now, it will help with catagorical values)
-model = LogisticRegression(random_state=0 , solver='lbfgs', multi_class='auto')
+model = DecisionTreeClassifier(max_depth=4,min_samples_split=2,min_samples_leaf=1)
 
 # give it the training data for it to start learning from it
-model.fit(x_train, train_y)
+model.fit(x_train, y_train)
 
 # use the model to output predictions and see if it did well
-y_pred = model.predict(x_val)
+y_pred = model.predict(x_test)
 
-# Predict probabilities
-probs_y = model.predict_proba(x_val)
-### Print results in pretty way
-probs_y = np.round(probs_y, 2)
-res = "{:<10} | {:<10} | {:<10} | {:<13} | {:<5}".format("train_y", "y_pred", "Setosa(%)", "versicolor(%)", "virginica(%)\n")
-res += "-"*65+"\n"
-res += "\n".join("{:<10} | {:<10} | {:<10} | {:<13} | {:<10}".format(x, y, a, b, c) for x, y, a, b, c in zip(train_y, y_pred, probs_y[:,0], probs_y[:,1], probs_y[:,2]))
-res += "\n"+"-"*65+"\n"
-print(res)
-
-# plot the data that you have , and see why the model made certian choices
-Iris_dataset.plot(kind ="scatter", x = "sepal_length", y = "sepal_width", c = "petal_length")
-plt.grid(True)
-
-iris = sns.load_dataset("iris")
-sns.set_style("whitegrid")
-sns.FacetGrid(iris, hue = "species", height = 6).map(plt.scatter,
-                                                        'sepal_length',
-                                                             'sepal_width').add_legend()
-
-
+# Visualize the trained tree
+plt.figure(figsize=(12, 8))
+plot_tree(model,
+          feature_names=x.columns,
+          class_names=model.classes_,  # unique class labels the model has seen during training
+          filled=True,
+          rounded=True)
+plt.title("Decision Tree Trained on Train/Test Split")
 plt.show()
--- Iris_code_only.py
+++ Iris_code_only.py
@@ -1,39 +1,22 @@
-from kaggle.api.kaggle_api_extended import KaggleApi
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-import pandas as pd
-import numpy as np
+from Helper.AI_Helper import *
+from sklearn.tree import DecisionTreeClassifier , plot_tree
 
 
-
-api = KaggleApi()
-api.authenticate()
-
-# I didn't do the downloading part again because I already have the dataset in my directory
-Iris_dataset = pd.read_csv('iris.csv')
-
+Iris_dataset = Get_Dataset('himanshunakrani/iris-dataset', 'iris.csv')
 y = Iris_dataset['species']
 x = Iris_dataset.drop('species', axis=1)
 
-x_train , x_val , train_y , val_y = train_test_split(x, y, test_size=0.2, random_state=0)
 
-x_train = StandardScaler().fit_transform(x_train)
-x_val = StandardScaler().fit_transform(x_val)
+x_train , x_test , y_train , y_test = train_test_split(x, y, test_size=0.2, random_state=0,stratify=y)
+model = DecisionTreeClassifier(max_depth=4,min_samples_split=2,min_samples_leaf=1)
+model.fit(x_train, y_train)
+y_pred = model.predict(x_test)
 
-model = LogisticRegression(random_state=0 , solver='lbfgs', multi_class='auto')
-model.fit(x_train, train_y)
-
-y_pred = model.predict(x_val)
-
-probs_y = model.predict_proba(x_val)
-probs_y = np.round(probs_y, 2)
-res = "{:<10} | {:<10} | {:<10} | {:<13} | {:<5}".format("train_y", "y_pred", "Setosa(%)", "versicolor(%)", "virginica(%)\n")
-res += "-"*65+"\n"
-res += "\n".join("{:<10} | {:<10} | {:<10} | {:<13} | {:<10}".format(x, y, a, b, c) for x, y, a, b, c in zip(train_y, y_pred, probs_y[:,0], probs_y[:,1], probs_y[:,2]))
-res += "\n"+"-"*65+"\n"
-print(res)
-
-
-
-
+plt.figure(figsize=(12, 8))
+plot_tree(model,
+          feature_names=x.columns,
+          class_names=model.classes_,
+          filled=True,
+          rounded=True)
+plt.title("Decision Tree Trained on Train/Test Split")
+plt.show()
